037ce835437cc92afb33a00fbe042df7be5eb795
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-01-13 22:39:24 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    //write your code here
+
+    return 0;
+
+}
