0802d7fff569ec65a9415076479d8885e8d60252
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-03-20 13:57:10 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,20 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
 using namespace std;
+#define _fast_ ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long 
+#define ull unsigned long long
+#define mod 1e9
+#define pb(x) push_back(x)
+#define eb(x) emplace_back(x)
+#define gcd(a,b) __gcd(a,b)
+#define count_ones(x) __builtin_popcount(x)
 
-#define MOD 1000000007
-#define pb push_back
 
-typedef long long ll;
-typedef unsigned long long ull;
+
 
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-
+    _fast_;
     //write your code here
-
     return 0;
-
 }
+
